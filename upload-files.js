f19395a948a258bf4c5791e75f5ea5de5b0175aa
Stephen Muraya <gathaiya28@gmail.com>
2021-04-24 18:56:03 +0300

    UPDATE 1.5.1
    Touches here and there

--- upload-files.js
+++ upload-files.js
@@ -1,16 +1,3 @@
-const id_file = document.querySelector("#id_copy");
-id_file.addEventListener("change", (e) => {
-  // Get the selected file
-  const [file] = e.target.files;
-  // Get the file name and size
-  const { name: fileName, size } = file;
-  // Convert size in bytes to kilo bytes
-  const fileSize = (size / 1000).toFixed(2);
-  // Set the text content
-  const fileNameAndSize = `${fileName} - ${fileSize}KB`;
-  document.querySelector(".upload-file-info-id").textContent = fileNameAndSize;
-});
-
 const kra_file = document.querySelector("#kra_upload");
 kra_file.addEventListener("change", (e) => {
   // Get the selected file
@@ -22,4 +9,17 @@ kra_file.addEventListener("change", (e) => {
   // Set the text content
   const fileNameAndSize = `${fileName} - ${fileSize}KB`;
   document.querySelector(".upload-file-info-kra").textContent = fileNameAndSize;
+});
+
+const id_file = document.querySelector("#id_copy");
+id_file.addEventListener("change", (e) => {
+  // Get the selected file
+  const [file] = e.target.files;
+  // Get the file name and size
+  const { name: fileName, size } = file;
+  // Convert size in bytes to kilo bytes
+  const fileSize = (size / 1000).toFixed(2);
+  // Set the text content
+  const fileNameAndSize = `${fileName} - ${fileSize}KB`;
+  document.querySelector(".upload-file-info-id").textContent = fileNameAndSize;
 });
